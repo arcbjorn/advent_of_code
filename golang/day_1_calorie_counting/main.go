a922ec56e7e5a01a1e1d9d6030488707f48e55ff
arcbjorn <arky.happ@gmail.com>
2022-12-03 15:50:00 -0300

    feat(golang/day_1): add first hald solution

--- golang/day_1_calorie_counting/main.go
+++ golang/day_1_calorie_counting/main.go
@@ -1,1 +1,66 @@
-package day_1_calorie_counting
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func getPacks(filename string) []int {
+	var packs []int
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		return packs
+	}
+
+	defer file.Close()
+
+	var line string
+	var amount int
+
+	var sum int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line = scanner.Text()
+
+		if line == "" {
+			packs = append(packs, sum)
+			sum = 0
+
+		} else {
+			amount, err = strconv.Atoi(line)
+			if err != nil {
+				panic(err)
+			}
+
+			sum += amount
+		}
+	}
+
+	return packs
+}
+
+func findMaxPack(packs []int) int {
+	var maxAmount int
+
+	for _, amount := range packs {
+		if amount > maxAmount {
+			maxAmount = amount
+		}
+	}
+
+	return maxAmount
+}
+
+func main() {
+	filename := os.Args[2]
+	packs := getPacks(filename)
+	maxPack := findMaxPack(packs)
+
+	// 66487
+	fmt.Println(maxPack)
+}
