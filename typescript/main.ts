10d0f74845c80684bbf15e7a794634b4505c7b05
arcbjorn <arky.happ@gmail.com>
2022-12-03 13:48:08 -0300

    feat(main.ts): add basic example & file read

---
+++ typescript/main.ts
@@ -0,0 +1,17 @@
+import { copy } from "https://deno.land/std@0.167.0/streams/copy.ts";
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
+}
+
+const filenames = Deno.args;
+for (const filename of filenames) {
+  const file = await Deno.open(filename);
+  await copy(file, Deno.stdout);
+  file.close();
+}
